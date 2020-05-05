985c5bcb51cc7eba9c73d2340894929c1269491b
Michael Seaton <mseaton@pih.org>
2020-05-04 22:51:29 -0400

    Moving docker files into a new repository that can be shared and published to docker hub

--- openmrs-server/build.sh
+++ openmrs-server/build.sh
@@ -6,9 +6,9 @@ NAMESPACE="${DOCKERHUB_NAMESPACE}"
 REPOSITORY="${DOCKERHUB_REPOSITORY}"
 
 GIT_COMMIT=`git log --format="%h" -n 1`
-GIT_BRANCH_OR_TAG=`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`
+GIT_REF=`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`
 
-echo "Building Docker image for branch/tag $GIT_BRANCH_OR_TAG with commit hash $GIT_COMMIT"
+echo "Building Docker image from $GIT_REF with commit hash $GIT_COMMIT"
 
 GIT_COMMIT_TAG="${NAMESPACE}/${REPOSITORY}:${GIT_COMMIT}"
 docker build -t ${GIT_COMMIT_TAG} .
@@ -16,16 +16,17 @@ docker build -t ${GIT_COMMIT_TAG} .
 echo "Docker image created"
 echo "Image tagged as ${GIT_COMMIT_TAG}"
 
-echo "Checking github.  Current branch/tag: $GIT_BRANCH_OR_TAG"
-if [ "$GIT_BRANCH_OR_TAG" == "master" ]; then
+echo "Checking github.  Current ref: $GIT_REF"
+if [ "$GIT_REF" == "master" ]; then
   echo "This is a build of the master branch.  Setting version tag to latest"
-  GIT_BRANCH_OR_TAG="latest"
+  GIT_REF="latest"
 fi
-docker tag ${GIT_COMMIT_TAG} ${GIT_BRANCH_OR_TAG}
+GIT_REF_TAG="${NAMESPACE}/${REPOSITORY}:${GIT_REF}"
+docker tag ${GIT_COMMIT_TAG} ${GIT_REF_TAG}
 
-echo "Image tagged as ${GIT_BRANCH_OR_TAG}"
+echo "Image tagged as ${GIT_REF_TAG}"
 
 echo "Logging into Docker Hub"
 docker login -u ${USER} -p ${PASSWORD}
 #docker push ${GIT_COMMIT_TAG}
-#docker push ${GIT_BRANCH_OR_TAG}
+#docker push ${GIT_REF_TAG}
